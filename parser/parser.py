1b63ee04891b73ea038466f69c8a873370f794c0
ArbuduImran <imran.arbudu.07@gmail.com>
2022-11-02 10:23:38 +0600

    hw6

--- parser/parser.py
+++ parser/parser.py
@@ -3,11 +3,11 @@ from pprint import pprint
 import requests
 from bs4 import BeautifulSoup as BS
 
-URL = "https://doramy.club/"
+URL = "https://www.securitylab.ru/news/"
 
 HEADERS = {
-    'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
 }
 
 
@@ -18,40 +18,34 @@ def get_html(url, params=''):
 
 def get_data(html):
     soup = BS(html, 'html.parser')
-    items = soup.find_all("div", class_="post-home")
-    dorama = []
+    items = soup.find_all("a", class_="article-card inline-card")
+    articles = []
     for item in items:
-        # datetime = item.find("time").getText()
-        dorama.append({
-            "title": item.find('div', class_="post-home").find('a').getText()
+        datetime = item.find("time").getText()
+        articles.append({
+            "link": "https://securitylab.ru" + item.get("href"),
+            "title": item.find("h2", class_="article-card-title").getText(),
+            "time": datetime[:5],
+            "day": datetime[8:].split(", ")[0],
+            "year": datetime[8:].split(", ")[1]
         })
-    pprint(dorama)
+    return articles
 
-html = get_html(URL)
-get_html(html.text)
 
-    #         "link": item.find('div', class_="post-home").find('a').getText('href'),
-    #         "time": datetime[:5],
-    #         "day": datetime[8:].split(", ")[0],
-    #         "year": datetime[8:].split(", ")[1]
-    #     })
-#     # return dorama
-#
-#
-# def parser():
-#     html = get_html(URL)
-#     if html.status_code == 200:
-#         dorama = []
-#         for i in range(1, 3):
-#             html = get_html(f'{URL}page/{i}/')
-#             current_page = get_data(html.text)
-#             dorama.extend(current_page)
-#         return dorama
-#     else:
-#         pass
+def parser():
+    html = get_html(URL)
+    if html.status_code == 200:
+        answers = []
+        for i in range(1, 2):
+            html = get_html(URL+f"page1_{i}.php")
+            current_page = get_data(html.text)
+            answers.extend(current_page)
+        return answers
+    else:
+        raise Exception("Error in parser")
 
-#
-# if __name__ == '__main__':
-#     # print(0)
-#     # print(html.text)
-#     pprint(parser())
+
+if __name__ == '__main__':
+    # print(0)
+    # print(html.text)
+    pprint(parser())
